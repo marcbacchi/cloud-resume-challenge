843fa55cb78061a52402a8a7ce5989385481289d
bacchimarc <bacchimarc@pm.me>
2022-02-24 13:18:01 -0700

    changing back, cant get it to work still.

--- cypress/integration/cypress-tests.js
+++ cypress/integration/cypress-tests.js
@@ -1,7 +1,7 @@
 describe('response status is 200', () => {
     it('GET', () => {
-        cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/').then((res) => {
-            expect(res).to.have.property('status', 200)
+        cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/')
+        .then((res) => {expect(res).to.have.property('status', 200)
         })        
     })
 })
@@ -9,28 +9,14 @@ describe('response status is 200', () => {
 describe('response body is not null', () => {
     it('GET', () => {
         cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/')
-        .then((res) => {
-            expect(res.body).to.not.be.null
+        .then((res) => {expect(res.body).to.not.be.null
         })        
-    })
-})
-
-describe('response body is not null', () => {
-    it('GET', () => {
-        cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/')
-        .then((res) => {
-            return new Promise(resolve => {        
-                expect(res.body).property('count').to.be.greaterThan(0)
-            })
-        })
     })
 })
 
 // describe('response body count element is positive number', () => {
 //     it('GET', () => {
 //         cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/').then((res) => {
-//             // expect(res).to.have.property('status', 200)
-//             // expect(res.body).to.not.be.null
 //             expect(res.body.count).to.be.greaterThan(0)
 //         })        
 //     })
