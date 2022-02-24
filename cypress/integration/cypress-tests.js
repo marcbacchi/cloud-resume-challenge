c2c063ed8982258d8757d27365e894d83f9395bd
bacchimarc <bacchimarc@pm.me>
2022-02-24 12:39:21 -0700

    testing updates

--- cypress/integration/cypress-tests.js
+++ cypress/integration/cypress-tests.js
@@ -1,9 +1,29 @@
-describe('api-test', () => {
+describe('response status is 200', () => {
     it('GET', () => {
         cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/').then((res) => {
             expect(res).to.have.property('status', 200)
+            // expect(res.body).to.not.be.null
+            // expect(res.body).to.be.greaterThan(0)
+        })        
+    })
+})
+
+describe('response body is not null', () => {
+    it('GET', () => {
+        cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/').then((res) => {
+            // expect(res).to.have.property('status', 200)
             expect(res.body).to.not.be.null
-            expect(res.body).to.be.greaterThan(0)
+            // expect(res.body).to.be.greaterThan(0)
+        })        
+    })
+})
+
+describe('response body count element is positive number', () => {
+    it('GET', () => {
+        cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/').then((res) => {
+            // expect(res).to.have.property('status', 200)
+            // expect(res.body).to.not.be.null
+            expect(res.body.count).to.be.greaterThan(0)
         })        
     })
 })
