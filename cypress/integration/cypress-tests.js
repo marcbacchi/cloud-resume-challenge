409a2de7a6be2376f024bf69c8615cfd6ea57a87
bacchimarc <bacchimarc@pm.me>
2022-02-24 12:25:28 -0700

    modifying Cypress tests

--- cypress/integration/cypress-tests.js
+++ cypress/integration/cypress-tests.js
@@ -3,7 +3,7 @@ describe('api-test', () => {
         cy.request('GET', 'https://snbeteyoeg.execute-api.us-east-1.amazonaws.com/').then((res) => {
             expect(res).to.have.property('status', 200)
             expect(res.body).to.not.be.null
-            // expect(res.body).to.be.a('string')
+            expect(res.body).to.be.greaterThan(0)
         })        
     })
 })
