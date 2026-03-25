2f7a401339c1d7ad762cfd12bf6bbd2bc09cff9c
Marc B <98762800+marcbacchi@users.noreply.github.com>
2026-03-25 15:12:27 -0700

    Refactor S3 bucket configuration and policies

--- s3.tf
+++ s3.tf
@@ -1,8 +1,17 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-  acl = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+
+  tags = var.common_tags
+}
+
+resource "aws_s3_bucket_acl" "www_bucket_acl" {
+  bucket = aws_s3_bucket.www_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "www_bucket_cors" {
+  bucket = aws_s3_bucket.www_bucket.id
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -10,21 +19,39 @@ resource "aws_s3_bucket" "www_bucket" {
     allowed_origins = ["*"]
     max_age_seconds = 10
   }
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-    # I need to build an 404 page in HTML 
+resource "aws_s3_bucket_website_configuration" "www_bucket_website" {
+  bucket = aws_s3_bucket.www_bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
 
-  tags = var.common_tags
+  error_document {
+    key = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "www_bucket_policy" {
+  bucket = aws_s3_bucket.www_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
 }
 
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-  acl = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+
+  tags = var.common_tags
+}
+
+resource "aws_s3_bucket_acl" "root_bucket_acl" {
+  bucket = aws_s3_bucket.root_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "root_bucket_cors" {
+  bucket = aws_s3_bucket.root_bucket.id
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -32,10 +59,18 @@ resource "aws_s3_bucket" "root_bucket" {
     allowed_origins = ["*"]
     max_age_seconds = 10
   }
+}
 
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
+resource "aws_s3_bucket_website_configuration" "root_bucket_website" {
+  bucket = aws_s3_bucket.root_bucket.id
+
+  redirect_all_requests_to {
+    host_name = "https://www.${var.domain_name}"
+    protocol  = "https"
   }
+}
 
-  tags = var.common_tags
+resource "aws_s3_bucket_policy" "root_bucket_policy" {
+  bucket = aws_s3_bucket.root_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
 }
