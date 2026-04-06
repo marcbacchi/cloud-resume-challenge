c465b5368fc60a560e97df6b90b3dfa000544bc1
Marc B <98762800+marcbacchi@users.noreply.github.com>
2026-04-05 19:15:57 -0700

    updates (#64)

--- dynamodb.tf
+++ dynamodb.tf
@@ -24,20 +24,17 @@ resource "aws_dynamodb_table" "dynamodbtable" {
     read_capacity      = 1
     projection_type    = "KEYS_ONLY"
   }
-
 }
 
 resource "aws_dynamodb_table_item" "tableitem" {
   table_name = aws_dynamodb_table.dynamodbtable.name
   hash_key   = aws_dynamodb_table.dynamodbtable.hash_key
-
   item = <<ITEM
 {
   "id": {"N": "0"},
   "visitcount": {"N": "0"}
 }
 ITEM
-
   lifecycle {
     ignore_changes = [item]
   }
